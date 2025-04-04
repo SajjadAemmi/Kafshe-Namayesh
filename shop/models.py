44f335890ef78940c4ccc8d0f74278359e44a7f9
Sajjad Aemmi <sajjadaemmi@gmail.com>
2025-04-04 19:38:13 +0330

    about and contact added to footer

--- shop/models.py
+++ shop/models.py
@@ -33,6 +33,7 @@ class Shoe(models.Model):
     details = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_date = models.DateField(null=True)
+    stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
